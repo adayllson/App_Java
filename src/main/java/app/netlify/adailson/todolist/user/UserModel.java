4b65c92e8c7131000372242c3b9604c5987a18a6
Adailson <adayllson.almeida@gmail.com>
2023-10-10 11:13:47 -0300

    adicionando dependências do Lombok

--- src/main/java/app/netlify/adailson/todolist/user/UserModel.java
+++ src/main/java/app/netlify/adailson/todolist/user/UserModel.java
@@ -1,35 +1,15 @@
 package app.netlify.adailson.todolist.user;
 
+import lombok.Data;
+
+//Notação lombok para inserir os getter e setters
+@Data
 public class UserModel {
     
     private String username;
     private String name;
     private String password;
 
-    //Método que insere o valor
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    //Método que busca o valor
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    
 }
 
