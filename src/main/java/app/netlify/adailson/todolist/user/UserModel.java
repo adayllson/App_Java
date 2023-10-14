ba33ed0a904566a2cc4b1b96ed9c174edc42f641
Adailson <adayllson.almeida@gmail.com>
2023-10-14 17:23:42 -0300

    alteração na controller e validação do username

--- src/main/java/app/netlify/adailson/todolist/user/UserModel.java
+++ src/main/java/app/netlify/adailson/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,6 +20,8 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
+    //Notação que define que a coluna no banco tenha um valor único
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
